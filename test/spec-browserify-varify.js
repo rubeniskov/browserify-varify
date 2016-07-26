d5ccef0ae04e3264c8c2a002f35af2e8c32bd1bf
Rubén López Gómez <loslopez1@icloud.com>
2016-07-26 17:50:30 +0200

    update: unit test

--- test/spec-browserify-varify.js
+++ test/spec-browserify-varify.js
@@ -1,51 +1,45 @@
 'use strict'
 /*jshint asi:true*/
 
-var test = require('tap').test
-var through = require('through')
-var browserify = require('browserify')
-
-test('replaces all consts that are declare identifiers, but leaves others intact', function (t) {
-  t.plan(1)
-
-
-
-})
-
-var expect = require("chai").expect;
-var varify = require("..");
+var through = require('through'),
+    browserify = require('browserify'),
+    expect = require("chai").expect,
+    varify = require("..");
 
 describe('Browserify Varify', function() {
 
     it('replaces all consts that are declare identifiers, but leaves others intact', function() {
         browserify()
-          .transform(require('..'))
-          .add(__dirname + '/../example/sample.js')
-          .bundle()
-          .pipe(through(ondata, onend));
+            .transform(require('..'))
+            .add(__dirname + '/../example/sample.js')
+            .bundle()
+            .pipe(through(ondata, onend));
 
         var data = ''
-        function ondata(d) { data += d }
-        function onend() {
-          var lines = data.split('\n')
-          var startline = lines.indexOf('// sample start') + 1
-            , endline = lines.indexOf('// sample end');
 
-          t.deepEqual(
-              lines.slice(startline, endline)
-            , [ "// this should be changed",
-                "var a = 1;",
-                "",
-                "// below two shouldn't",
-                "var keep = { const: 1 };",
-                "keep.const = 2;",
-                "",
-                "// only function assignment should be changed",
-                "var foo = function () {",
-                "  console.log('cannot change me');",
-                "  console.log('some const s should be left unchanged');",
-                "};" ]
-          )
+        function ondata(d) {
+            data += d
+        }
+
+        function onend() {
+            var lines = data.split('\n')
+            var startline = lines.indexOf('// sample start') + 1,
+                endline = lines.indexOf('// sample end');
+
+            expect(lines.slice(startline, endline)).to.deep
+                .equal(["// this should be changed",
+                    "var a = 1;",
+                    "",
+                    "// below two shouldn't",
+                    "var keep = { const: 1 };",
+                    "keep.const = 2;",
+                    "",
+                    "// only function assignment should be changed",
+                    "var foo = function () {",
+                    "  console.log('cannot change me');",
+                    "  console.log('some const s should be left unchanged');",
+                    "};"
+                ]);
         }
     });
 });
